7a86012955702d0b7fae28eae7ac6a6939cfaa45
Naraoi <35643120+Naraoi@users.noreply.github.com>
2025-10-19 18:11:15 +0800

    Create prompts.py

---
+++ prompts.py
@@ -0,0 +1,42 @@
+prompts = [
+    [
+        "masterpiece,best quality,1girl,solo,long hair,looking at viewer,red eyes,bangs,shirt,blush,collarbone,white shirt,upper body,smile,closed mouth,hair between eyes,white hair,hair ornament,",
+        "low quality,blurry,lowres,worstquality,normal quality,bad proportions,out of focus,logo,text,twisted limbs,",
+    ],
+    [
+        "1girl,animal ears,tail,cat ears,green eyes,solo,cat tail,thighhighs,hair ornament,short hair,brown hair,bell,dress,fang,heart hands,open mouth,wings,cat girl,full body,ribbon,twintails,",
+        "bad anatomy,worst quality,low quality,blurry,monochrome,comic,greyscale,text,watermark,multiple views,pubic hair,sketches,fused fingers,fused toes,bad feet,missing toe,extra toes,missing limb,missing finger,extra fingers,ugly hands,deformed hands,fused hands,fused eyes,liquid eyes,fused pupils,liquid pupils,censored,mosaic,minigirl,short hair,very short hair,EasyNegativeV2,AnimeSupport-neg,badhandv4,",
+    ],
+    [
+        "1girl,bangs,beret,blush,braid,dress,eyebrows visible through hair,full body,hair between eyes,hair ornament,hairclip,hat,long hair,long sleeves,looking at viewer,parted lips,pink hair,red headwear,shoes,solo,standing,sweater,twin braids,very long hair,white legwear,x hair ornament,outdoors,",
+        "2boy,lowres,bad anatomy,bad hands,text,error,missing fingers,extra digit,fewer digits,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,username,blurry,lowres graffiti,low quality lowres simple background,multiple people,Multiple people,bad body,ugly,poorly drawn face,malformed hands,poorly drawn hands,mutated hands,too many fingers,mutated hands and fingers,extra legs,extra limb,extra arms,disconnected limbs,monochrome,bad proportions,worstquality,grayscale,long body,EasyNegativeV2,",
+    ],
+    [
+        "solo,1girl,looking at viewer,Noelle,blush,full body,black_shoes,blush,laughing,hand up,teasing_smile,kind_smile,white gloves,white dress,bathtub,wet,",
+        "badhandv4,BadNegAnatomyV1-neg,Multiple people,bad hands,malformed hands,mutated hands,missing fingers,fused fingers,floating limbs,malformed limbs,low quality,bad anatomy,lowres,sweat,bad proportions,poorly drawn hands,too many fingers,mutated hands and fingers,text,",
+    ],
+    [
+        "1girl,lingjia,pink hair,blue eyes,big eyes,uniforms,standing,hat,round face,kawaii,round face, masterpiece, best quality,",
+        "badhandv4,EasyNegativeV2,ng_deepnegative_v1_75t,EasyNegative,, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry",
+    ],
+    [
+        "Satou, happy, blushing, distant view, portrait, , raining, outside, wet clothes, emotionless, expressionless, dreary atmosphere, sad atmosphere",
+        "easynegative, closed eyes, expressionless, emotionless, angry, sad, indoors, school, apartment, side view, rear view,  bad hands, weird hands,",
+    ],
+    [
+        "unparalleled masterpiece, masterpiece, best quality, high quality, absurdres, 1girl, maniwa roka, wa maid, pinstripe kimono, kimono, apron, hair bow, striped kimono, long hair, hair between eyes, frilled apron, frilled sleeves, mole under eye, aqua eyes",
+        "easynegative, bad-hands-5, extra fingers, fewer fingers, low quality, worst quality, bad anatomy, inaccurate limb, bad composition, inaccurate eyes, extra digit,fewer digits, extra arms, medium quality, deleted, lowres, comic, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, jpeg artifacts, signature, watermark, username, blurry, negative space, red headband, less fingers, deformed hands, polydactyl, chromatic abberation, deformed limbs, extra limbs, fewer fingers, extra fingers, crooked fingers, loli, shota, twintails, cleavage cutout",
+    ],
+    [
+        "masterpiece, best quality, ultra-detailed, realistic, 8k, one pine tree, flowers fields, many kinds of flowers, a couple of rabbits on very distant, grass, glitter, midnight, dark vibe, dark light, dim light, rim light, stars, natural night light, fruits trees, big fruits trees, a lots of fruits trees, out-of-this-world scenery, lifelike, naturalism, greatest naturalism photo, immaculate detailed realism photography, rich colors, best colors perfect composition, perfect colors, colorful, clear photo, intrigue nature panorama, delicate nature photo, immaculate micro-details nature elements, out-of-this-world scenery, landscape, flowers, crystal fantastic, breathtaking beautiful nature, unmatched beauty nature, more nature elements, richest nature elements, aesthetic, artistic style, surreal, insanely lifelike, top best naturalism photography, greatest detailed, intricate details, depth of field, no human, detailerlora, a lots kinds of flowers, JZCG001,",
+        "negative_hand, NegfeetV2, Realisian-Neg, worst quality, low quality, normal quality, poorly drawn, lowres, low resolution, signature, watermarks, ugly, out of focus, error, blurry, unclear photo, bad photo, unrealistic, semi realistic, pixelated, cartoon, anime, cgi, drawing, bra, panties, dress, shirt, clothes, small breast, medium breast, extra limbs, moon, milky way, 2d, 3d, censored,",
+    ],
+    [
+        "Photorealism, cinematic film still, a woman in a dress is dancing in the living room with an antique radio in the background, Elizabeth Polunin, cinematic photography, a photorealistic painting, arabesque, Fujifilm Superia Venus 800 rendered in unreal engine, photorealistic",
+        "anime, cartoon, muscular, simplified, unrealistic, impressionistic, low resolution, earthly, mundane, simple, simplified, abstract, unrealistic, impressionist, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured, figuration-libre, easynegative",
+    ],
+    [
+        "masterpiece, best quality, ultra-detailed, realistic, 8k, capture the most beautiness nature in a glowing morning dew crystal, a pine tree, flowers fields, many kinds of flowers, a couple of rabbits on very distant, lakes, meadows, glitter, moonlight, out-of-this-world scenery, lifelike, naturalism, greatest naturalism photo, immaculate detailed realism photography, rich colors, best colors perfect composition, perfect colors, colorful, clear photo, intrigue nature panorama, delicate nature photo, immaculate micro-details nature elements, out-of-this-world scenery, landscape, flowers, crystal fantastic, breathtaking beautiful nature, unmatched beauty nature, more nature elements, rich nature elements, aesthetic, artistic style, surreal, insanely lifelike, top best naturalism photography, greatest detailed, intricate details, depth of field, no human, detailerlora,",
+        "negative_hand, NegfeetV2, Realisian-Neg, worst quality, low quality, normal quality, poorly drawn, lowres, low resolution, signature, watermarks, ugly, out of focus, error, blurry, unclear photo, bad photo, unrealistic, semi realistic, pixelated, cartoon, anime, cgi, drawing, bra, panties, dress, shirt, clothes, small breast, medium breast, extra limbs, 2d, 3d, censored, girl, human, woman, human,",
+    ],
+]
